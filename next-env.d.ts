f3f4f69ae815486777f2249e1590834f01a5e623
Fernando Medina <fermmm@gmail.com>
2020-10-06 18:52:52 -0300

    Installed typescript, fixed config issues when js and ts are in the same next.js project and installed mui and styled-components

---
+++ next-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+declare module "*.css" {
+  export default "" as string;
+}
+declare module "*.png" {
+  export default "" as string;
+}
+declare module "*.svg" {
+  export default "" as string;
+}
