9c8d6e5f075de4e6298cab5b556b2d4ea3b3c1de
Fernando Medina <fermmm@gmail.com>
2020-10-22 22:03:38 -0300

    Implemented About section

---
+++ components/pages/About/styles.About.ts
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+import { Column } from '../../ui/Column/Column';
+import { Button } from '../../ui/Button/Button';
+import { Row } from 'components/ui/Row/Row';
+
+export const AboutContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+
+  @media ${props => props.theme.queries.tabletXL} {
+    flex-direction: column;
+  }
+`;
+
+export const ProjectDescriptionContainer = styled(Column)`
+  max-width: 50%;
+  margin-right: 96px;
+
+  @media ${props => props.theme.queries.tabletXL} {
+    margin-right: 0px;
+    max-width: unset;
+    margin-bottom: 54px;
+  }
+`;
+
+export const TandaTypesContainer = styled(Column)`
+  max-width: 50%;
+
+  @media ${props => props.theme.queries.tabletXL} {
+    max-width: unset;
+  }
+`;
+
+export const TextBlock = styled('div')`
+  font-size: 21px;
+  letter-spacing: -0.014em;
+  line-height: 27px;
+  margin-bottom: 22px;
+  color: ${props => props.theme.colors.text5};
+
+  span {
+    color: ${props => props.theme.colors.text4};
+    font-weight: 600;
+  }
+
+  @media ${props => props.theme.queries.tabletXL} {
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 20px;
+  }
+`;
+
+export const ButtonConnectWallet = styled(Button)`
+  min-width: 317px;
+  height: 45px;
+  margin-top: 30px;
+
+  &.MuiButton-root {
+    padding: 0 48px;
+
+    @media ${props => props.theme.queries.tabletXL} {
+      padding: 0 3px;
+    }
+
+    .MuiButton-label {
+      font-size: 24px;
+
+      @media ${props => props.theme.queries.tabletXL} {
+        font-size: 21px;
+      }
+    }
+  }
+
+  @media ${props => props.theme.queries.tabletXL} {
+    width: 232px;
+    height: unset;
+  }
+`;
+
+export const TandaType = styled(Row)`
+  margin-bottom: 0px;
+`;
+
+export const IconTanda = styled('img')`
+  height: 25px;
+  margin-right: 15px;
+`;
+
+export const Opacity = styled('div')`
+  opacity: 0.4;
+`;
