13d19d2702f641793c294e44d77b5432c72a93ce
Fernando Medina <fermmm@gmail.com>
2020-11-12 00:45:21 -0300

    Implemented tandas list as independen page

---
+++ components/pages/TandasList/TandasList.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+import { useTranslation } from 'i18n';
+import { TandasListContainer, TitlesContainer } from './styles.TandasList';
+import { Column } from '../../ui/Column/Column';
+import { TitleText } from '../../ui/TitleText/TitleText';
+import { TandaBasicPreview } from 'components/ui/TandaBasicPreview/TandaBasicPreview';
+import { TandaBasicPreviewItem } from '../../ui/TandaBasicPreview/TandaBasicPreviewItem/TandaBasicPreviewItem';
+import { useTandasBasicInfo } from 'hooks/useTandasBasicInfo';
+
+export const TandasList: FC = () => {
+  const { t } = useTranslation();
+  const tandas = useTandasBasicInfo();
+
+  return (
+    <TandasListContainer>
+      <Column>
+        <TitlesContainer>
+          <TitleText>{t('Welcome to CoinTanda!')}</TitleText>
+          <TitleText small>{t('You may join any of these Tandas')}</TitleText>
+        </TitlesContainer>
+        <TandaBasicPreview>
+          {tandas.map((tanda, i) => (
+            <TandaBasicPreviewItem
+              address={tanda.address}
+              darkBackground={i % 2 === 0}
+              iconColor={'gold'}
+              key={tanda.address}
+            />
+          ))}
+        </TandaBasicPreview>
+      </Column>
+    </TandasListContainer>
+  );
+};
---
+++ components/pages/TandasList/styles.TandasList.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const TandasListContainer = styled('div')``;
+
+export const TitlesContainer = styled('div')`
+  @media ${props => props.theme.queries.tabletXL} {
+    padding-left: 20px;
+  }
+`;
