2e4e7f8dca2f63f61edd69ff32693487f4aba139
galimba <galimba@thinkanddev.com>
2021-03-22 11:52:34 -0300

    FAQ refactored

--- components/pages/Faq/Faq.tsx
+++ components/pages/Faq/Faq.tsx
@@ -22,105 +22,125 @@ export const Faq: FC = () => {
           <FaqItem id="1">
             <TitleText medium>{t('What is CoinTanda?')}</TitleText>
             <TextBlock>
-              {t('CoinTanda is a')} <span>{t('social saving platform')}</span>.{' '}
-              {t('By joining a group of people called a Tanda')}, {t('you can win money')}.
+              {t('What is CoinTanda faq')}
             </TextBlock>
           </FaqItem>
           <FaqItem id="2">
             <TitleText medium>{t('What do I need to participate?')}</TitleText>
             <TextBlock>
-              {t('You need some')} <span>{t('cryptocoins')}</span>, {t('for this first version')}:{' '}
-              <span>{t('DOC')}</span> ({t('Dollar on Chain')}). {t('You also need a')} <span>{t('DeFi wallet')}</span>{' '}
-              {t('such as Nifty')}. {t('You will need this wallet')}, {t('Under')} <span>9</span>{' '}
-              {t('and')} <span>10</span> {t('we will explain')}.
+              {t('What do I need to participate faq')}
             </TextBlock>
           </FaqItem>
           <FaqItem id="3">
             <TitleText medium>{t('Do I have to entrust my money to you?')}</TitleText>
             <TextBlock>
-              {t('No We never')} <span>{t('full control')}</span> {t('of them. CoinTanda is based on')},{' '}
-              <span>{t('smart contracts')}</span> {t('and fully')} <span>{t('decentralized')}</span>.
+              {t('Do I have to entrust my money to you faq')}
             </TextBlock>
           </FaqItem>
           <FaqItem id="4">
             <TitleText medium>{t('What is a provably fair raffle?')}</TitleText>
             <TextBlock>
-              {t('In traditional gambling')} <span>{t('blockchain-based raffle')}</span>{' '}
-              {t('like CoinTanda, you are in control')} <span>{t('disclosed')}</span>, {t('and can be')}{' '}
-              <span>{t('checked')}</span> {t('by everyone')}
+              {t('What is a provably fair raffle faq')}
             </TextBlock>
           </FaqItem>
           <FaqItem id="5">
             <TitleText medium>{t('Where does the interest come from?')}</TitleText>
-            <TextBlock>{t('CoinTanda cooperates with decentralized')}</TextBlock>
+            <TextBlock>
+              {t('Where does the interest come from faq1')}
+              <br/>
+              <br/>
+              {t('Where does the interest come from faq2')}
+            </TextBlock>
           </FaqItem>
           <FaqItem id="6">
             <TitleText medium>{t('What are smart contracts?')}</TitleText>
             <TextBlock>
-              {t('A smart contract is a piece')} <span>{t('vending machine')}</span>.{' '}
-              {t('You insert the required')} <span>{t('automatically')}</span>{' '}
-              {t('deliver what is defined')} <span>{t('rules')}</span> {t('and penalties between')}{' '}
-              <span>{t('enforces')}</span> {t('them')}.
-              <br />
-              {t('In our case, the smart contract defines')}.
+              {t('What are smart contracts faq1')}
+              <br/>
+              <br/>
+              {t('What are smart contracts faq2')}.
             </TextBlock>
           </FaqItem>
           <FaqItem id="7">
             <TitleText medium>{t('Are smart contracts safe?')}</TitleText>
             <TextBlock>
-              {t('Smart contracts are definitely more')} <span>{t('trustworthy')}</span>{' '}
-              {t('than human beings, as they cannot')} <span>{t('open source')}</span> {t('and')}{' '}
-              <span>{t('audited')}</span>.
+              {t('Are smart contracts safe faq1')}
               <br />
-              {t('Open source software can be')}
-              .
+              <br/>
+              {t('Are smart contracts safe faq2')}
+              <br/>
               <br />
-              {t('Furthermore, our smart contracts have been thoroughly')} <span>{t('audited')}</span>{' '}
-              {t('by a neutral company which is specialized')}.
+              {t('Are smart contracts safe faq3')}
+            </TextBlock>
+          </FaqItem>
+          <FaqItem id="8">
+            <TitleText medium>{t('What are RSK smart contracts?')}</TitleText>
+            <TextBlock>
+              {t('What are RSK smart contracts faq1')}
+              <br/>
+              <br />
+              {t('What are RSK smart contracts faq2')}
             </TextBlock>
           </FaqItem>
         </LeftColumnContainer>
         <RightColumnContainer>
-          <FaqItem id="8">
-            <TitleText medium>{t('What are RSK smart contracts?')}</TitleText>
-            <TextBlock>
-              {t('CoinTanda works with smart contracts by')} <span>{t('RSK')}</span>{' '}
-              {t('which run on top of the')} <span>{t('Bitcoin')}</span> {t('blockchain')}.{' '}
-              {t('As the Bitcoin blockchain')}
-              .
-              <br />
-              {t('The only small downside')} <span>{t('Smart Bitcoins')}</span> {t('short RBTC')}.
-            </TextBlock>
-          </FaqItem>
           <FaqItem id="9">
             <TitleText medium>{t('How do I get a wallet?')}</TitleText>
             <TextBlock>
               {t('Currently there are')} <br />
               <IsolatedAreaContainer>
-                <Link href="https://chrome.google.com/webstore/detail/nifty-wallet/jbdaocneiiinmjbjlgalhcelgbejmnid">
+                <Link href="https://developers.rsk.co/wallet/use/nifty/">
                   {t('Download Nifty')}
                 </Link>
                 <br />
-                <Link href="https://metamask.io/download.html">{t('Download Metamask')}</Link>
+                <Link href="https://developers.rsk.co/wallet/use/metamask/">{t('Download Metamask')}</Link>
               </IsolatedAreaContainer>
-              {t('Make sure to save your')} <span>{t('12 recovery words')}</span>{' '}
-              {t('which you need to restore')}.
+              {t('Make sure to save your')}
             </TextBlock>
           </FaqItem>
           <FaqItem id="10">
             <TitleText medium>{t('How do I set up my wallet?')}</TitleText>
             <TextBlock>
-              {t('Before you can start')} <span>{t('RSK Mainnet')}</span>{' '}
-              {t('as your preferred network')}.
+              {t('Before you can start')}. {t('For testing CoinTanda, select')}
               <br />
-              {t('In Nifty, you click on')} <span>{t('Select Network')}</span> {t('and choose')}{' '}
+              <br/>
+              <span>{t('Set up Nifty')}:</span>
+              <br/>
+              {t('In Nifty, you click on')} {t('Select Network')} <span>{t('RSK Testnet')}</span> {t('or')}{' '}
               <span>{t('RSK Mainnet')}</span>.
+              <br />
+              <br />
+              <span>{t('Set up Metamask')}:</span>
               <br />
               {t('In Metamask, you click on')} <span>{t('Networks')}</span> {t('and select')}{' '}
               <span>{t('Custom RPC')}</span>.
               <br />
               {t('Here you need to fill')}
               :
+              <br />
+              <IsolatedAreaContainer>
+                <ul>
+                  <li>
+                    {t('Network Name')}: <span>{t('RSK Testnet')}</span>
+                  </li>
+                  <li>
+                    {t('New RPC URL')}:{' '}
+                    <Link href="https://public-node.testnet.rsk.co">https://public-node.testnet.rsk.co</Link>{' '}
+                  </li>
+                  <li>
+                    {t('Chain ID')}: <span>31</span>
+                  </li>
+                  <li>
+                    {t('Symbol')}: <span>T-{t('RBTC')}</span>
+                  </li>
+                  <li>
+                    {t('Block Explorer URL')}:{' '}
+                    <Link href="https://explorer.testnet.rsk.co/">https://explorer.testnet.rsk.co/</Link>{' '}
+                  </li>
+                </ul>
+              </IsolatedAreaContainer>
+              <br />
+              {t('The same procedure will work for')} <span>{t('RSK Mainnet')}</span> {t('after Betatest and audit')}
               <br />
               <IsolatedAreaContainer>
                 <ul>
@@ -143,8 +163,17 @@ export const Faq: FC = () => {
                   </li>
                 </ul>
               </IsolatedAreaContainer>
-              {t('From now on, you can simply select')} <span>{t('RSK Mainnet')}</span>{' '}
+              {t('From now on, you can simply select')} <span>{t('RSK Testnet')}</span> {t('or')} <span>{t('RSK Mainnet')}</span>{' '}
               {t('in the dropdown menu on top of the window')}.
+              <br />
+              <br/>
+              {t('To add Dollar-on-Chain')}
+              <br/>
+              {t('In the field')} <span>Token Contract Address</span> {t('you fill in')}:
+              <br/>
+              {t('For')} <span>{t('RSK Testnet')}</span>: 0xCb46C0DdC60d18eFEB0e586c17AF6Ea36452DaE0
+              <br/>
+              {t('For')} <span>{t('RSK Mainnet')}</span>: 0xe700691da7b9851f2f35f8b8182c69c53ccad9db
             </TextBlock>
           </FaqItem>
           <FaqItem id="11">
@@ -159,6 +188,25 @@ export const Faq: FC = () => {
               .
               <br />
               {t('Remember, by using CoinTanda')}.
+            </TextBlock>
+          </FaqItem>
+          <FaqItem id="12">
+            <TitleText medium>{t('What is Dollar-on-Chain')}</TitleText>
+            <TextBlock>
+              {t('Dollar-on-Chain (DOC) is a')}
+            </TextBlock>
+          </FaqItem>
+          <FaqItem id="13">
+            <TitleText medium>{t('How can I get some Dollar-on-Chain')}</TitleText>
+            <TextBlock>
+              {t('For the Betatest of CoinTanda, you will get DOCs on Testnet for free. Please fill in this form if you want to participate')}
+               <Link href="https://bit.ly/cointanda">https://bit.ly/cointanda</Link>.
+              <br/>
+              {t('You can receive free DOCs for testing here')}: 
+              <Link href="https://faucet.cointanda.com">https://faucet.cointanda.com</Link>
+              <br/>
+              {t('If you need more, you can get Smart Bitcoins here')}: <Link href="https://faucet.rsk.co ">https://faucet.rsk.co </Link>
+               {t('and mint new DOCs here')} <Link href="https://moc-testnet.moneyonchain.com">https://moc-testnet.moneyonchain.com</Link>
             </TextBlock>
           </FaqItem>
         </RightColumnContainer>
