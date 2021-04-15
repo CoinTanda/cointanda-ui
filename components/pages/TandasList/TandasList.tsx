6aba89f0417da384f38af2d31e5e3e0cf1d31da1
facu <facundogregori@hotmail.com>
2021-04-15 15:24:40 -0300

    replace href by link

--- components/pages/TandasList/TandasList.tsx
+++ components/pages/TandasList/TandasList.tsx
@@ -6,6 +6,8 @@ import { TitleText } from '../../ui/TitleText/TitleText';
 import { TandaBasicPreview } from 'components/ui/TandaBasicPreview/TandaBasicPreview';
 import { TandaBasicPreviewItem } from '../../ui/TandaBasicPreview/TandaBasicPreviewItem/TandaBasicPreviewItem';
 import { useTandasList } from 'hooks/useTandasList';
+import Link from 'next/link'
+
 
 export const TandasList: FC = () => {
   const { t } = useTranslation();
@@ -16,7 +18,7 @@ export const TandasList: FC = () => {
       <Column>
         <TitlesContainer>
           <TitleText>{t('Welcome to CoinTanda!')}</TitleText>
-          <TitleText small>{t('We are currently in beta mode')}. <a href="contact">{t('Please take part in our beta test')}</a></TitleText>
+          <TitleText small>{t('We are currently in beta mode')}. <Link href="/contact">{t('Please take part in our beta test')}</Link></TitleText>
           <TitleText small>{t('You may join any of these Tandas')}</TitleText>
         </TitlesContainer>
         <TandaBasicPreview>
